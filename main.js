d5f950967c3c25fdc35a4d96b4a873f61929fd13
avikant wadhwa <avikant16@hotmail.com>
2019-11-23 21:56:27 +0530

    set_up_event

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ function start() {
     document.turn = 'X';
 }
 
+function HandleClick(btnval) {
+
+    var btn = document.getElementById(btnval);
+    if (btn.innerText === '') {
+        btn.innerText = document.turn;
+       
