8fbde04b84bd40a1fde796ddba609cf0068e269e
avikant wadhwa <avikant16@hotmail.com>
2019-11-23 22:03:12 +0530

    function_ele_access

--- main.js
+++ main.js
@@ -21,4 +21,18 @@ function HandleClick(btnval) {
 
 }
 
+}
 
+function check_winner() {
+    var i = 0,f=true,j;
+    for (i = 7; i > 0; i -= 3) {
+        f=true;
+        var num = document.getElementById(i.toString());
+        for (j = i + 1; j < i + 3; j++) {
+            var btn = document.getElementById(j.toString());
+            if (btn.innerText !== num.innerText)
+                f=false;
+        }
+        if(f)
+            return true;
+   
