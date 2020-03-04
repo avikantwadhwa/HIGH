d5e046c40b98ed2cd11554f5cb91a3841bc104af
avikant wadhwa <avikant16@hotmail.com>
2020-03-05 05:00:00 +0530

    chnage_upload

---
+++ index.js
@@ -0,0 +1,13 @@
+(function() {
+    var $target = $('#drop-zone');
+
+    function dropZone($target, onDrop) {
+        $target.
+        bind('dragover', function() {
+            $target.addClass('drag-over');
+            return false;
+        }).
+        bind("dragend", function() {
+            $target.removeClass('drag-over');
+            return false;
+        }).
